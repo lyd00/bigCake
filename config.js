6c3185bd61b2f1542ad6b9500354f5cff6fccadd
lyd00 <yanda@vite.org>
2018-11-28 17:55:08 +0800

    Add config.js

--- config.js
+++ config.js
@@ -1,36 +1,3 @@
-var config = [{
-    "name": "陈思",
-    "addrs": [
-        "vite_ef3c95e83d0d1cbf694108a61b632591678cd656a264d32354"
-    ],
-    "earningsAddr": "vite_ef3c95e83d0d1cbf694108a61b632591678cd656a264d32354",
-    "pledgeAmount": 12.5 * 10000
-}, {
-    "name": "王莛岳",
-    "addrs": [
-        "vite_4c17335b318dd8caf4b380205a94dae80c1b675ebb7245200f",
-        "vite_e15b6fcb52d8f3b4aa80a8b67f1d6f907b1ea7cf8b5ba98e5c",
-        "vite_2fddb1f3bea3c3de04ec65ba26940b62f7a42f6280639139dd",
-        "vite_1384252652eac061df7333de3a74e6ff4d4afc7e57ccbdda9e",
-        "vite_20491cde8edb5f11b8fc0acfeae1fa943ff3efa069726a6a41"
-    ],
-    "earningsAddr": "vite_4c17335b318dd8caf4b380205a94dae80c1b675ebb7245200f",
-    "pledgeAmount": 12.5 * 10000
-}, {
-    "name": "袁章",
-    "addrs": [
-        "vite_61404d3b6361f979208c8a5c442ceb87c1f072446f58118f68"
-    ],
-    "earningsAddr": "vite_ef3c95e83d0d1cbf694108a61b632591678cd656a264d32354",
-    "pledgeAmount": 12.5 * 10000
-},
-{
-    "name": "李焱达",
-    "addrs": [
-        "vite_0149e4e16364002ab7681f15be6e2c7372f5986eed824ac8e6"
-    ],
-    "earningsAddr": "vite_ef3c95e83d0d1cbf694108a61b632591678cd656a264d32354",
-    "pledgeAmount": 12.5 * 10000
-}]
+let configJson = require('./config.json') 
 
-module.exports = config
+module.exports = configJson
