b58798433abb8d365e37742112e09232634306fb
hurrytospring <ddyz1993@163.com>
2018-11-29 21:10:10 +0800

    add settle

--- settle.js
+++ settle.js
@@ -1,19 +1,36 @@
-var allocate = require('./allocate')
-function receiveTotalEarnings() {
-    return 0 
+import { client, constant } from '@vite/vitejs';
+const { method } = constant;
+const { initClientWithIPC } = client;
+const clientInstance = new initClientWithIPC({
+    path: '~/.gvite/testdata/gvite.ipc',
+    delimiter = '\n',
+    timeout = 2000
+});
+
+// [TODO]
+async function receiveTotalEarnings() {
+    return await clientInstance.request(method.ledger.getAccountByAccAddr, 'selfAddr');
 }
 
-function createSendTx (earningsAddr, amount) {
-
+// [TODO]
+async function createSendTx(earningsAddr, amount) {
+    // send money
+    return await clientInstance.request(method.wallet.createTxWithPassphrase, {
+        selfAddr: '',
+        toAddr: earningsAddr,
+        tokenTypeId: 'tti_5649544520544f4b454e6e40', // VITE
+        passphrase: '',
+        amount: amount
+    });
 }
 
-function settle (dayDate, superNodeName) {
+function settle(dayDate, superNodeName) {
     allocate.calcAllocation(dayDate, superNodeName).then(function (allocation) {
         let totalEarnings = receiveTotalEarnings()
 
-        allocation.fundMembersVote.forEach(function (fundMember) { 
+        allocation.fundMembersVote.forEach(function (fundMember) {
             let earnings = totalEarnings * fundMember.voteRatio;
-    
+
             createSendTx(fundMember.earningsAddr, earnings)
         })
     })
